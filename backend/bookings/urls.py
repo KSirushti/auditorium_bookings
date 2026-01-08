7bd9b24267c9e5032ee6e989eb09e8d860f0ed93
KSirushti <156492843+KSirushti@users.noreply.github.com>
2026-01-08 21:38:07 +0530

    Update urls.py

--- backend/bookings/urls.py
+++ backend/bookings/urls.py
@@ -5,7 +5,6 @@ from .views import BookingListCreateView, BookingDetailView, approved_bookings, 
 urlpatterns = [
     path('bookings/', BookingListCreateView.as_view()),
     path("health/", health),
-    path("", home),
     path('bookings/<int:booking_id>/', BookingDetailView.as_view()),  # This enables PATCH
 path('api/approved-bookings/', approved_bookings, name='approved-bookings'),
 ]
